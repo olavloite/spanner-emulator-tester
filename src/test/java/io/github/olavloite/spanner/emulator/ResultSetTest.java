8c1536bb5848b04d600c5562511b13bced35a24a
Olav Loite <koloite@gmail.com>
2018-06-06 18:01:23 +0200

    added test for query outside transaction

---
+++ src/test/java/io/github/olavloite/spanner/emulator/ResultSetTest.java
@@ -0,0 +1,45 @@
+package io.github.olavloite.spanner.emulator;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import com.google.cloud.spanner.ResultSet;
+import com.google.cloud.spanner.SpannerException;
+import com.google.cloud.spanner.Statement;
+import com.google.cloud.spanner.TransactionContext;
+import com.google.cloud.spanner.TransactionRunner.TransactionCallable;
+
+public class ResultSetTest extends AbstractSpannerTest {
+
+  @Rule
+  public ExpectedException expected = ExpectedException.none();
+
+  @Before
+  public void before() {
+    createNumberTable();
+  }
+
+  @After
+  public void after() {
+    dropNumberTable();
+  }
+
+  private ResultSet rs;
+
+  @Test()
+  public void testSelectOutsideTransaction() {
+    getDatabaseClient().readWriteTransaction().run(new TransactionCallable<Void>() {
+      @Override
+      public Void run(TransactionContext transaction) throws Exception {
+        rs = transaction.executeQuery(Statement.of("select * from number"));
+        return null;
+      }
+    });
+    expected.expect(SpannerException.class);
+    expected.expectMessage("Transaction with id");
+    rs.next();
+  }
+
+}
